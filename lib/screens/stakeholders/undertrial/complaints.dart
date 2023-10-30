125f2851ef3180ac99fcbe692e14464cee545f52
amspsingh04 <sp.singh.150604@gmail.com>
2023-10-31 01:13:43 +0530

    ch4

--- lib/screens/stakeholders/undertrial/complaints.dart
+++ lib/screens/stakeholders/undertrial/complaints.dart
@@ -18,12 +18,6 @@ class Complaints extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Complaint'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.logout),
-          ),
-        ],
       ),
       body: Center(
         child: Column(
@@ -71,10 +65,8 @@ class ComplaintsForm extends StatefulWidget {
 }
 
 class _ComplaintsFormState extends State<ComplaintsForm> {
-  
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _deScriptionController = TextEditingController();
-  final TextEditingController _subCategoryController = TextEditingController();
   final TextEditingController _CategoryController = TextEditingController();
   FocusNode searchFocusNode = FocusNode();
   FocusNode textFieldFocusNode = FocusNode();
@@ -82,116 +74,66 @@ class _ComplaintsFormState extends State<ComplaintsForm> {
   late MultiValueDropDownController _cntMulti;
 
   @override
-  void initState(){
+  void initState() {
     _cnt = SingleValueDropDownController();
     _cntMulti = MultiValueDropDownController();
     super.initState();
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _cnt.dispose();
     _cntMulti.dispose();
     super.dispose();
   }
 
   bool _deScriptionFocused = false;
-  bool _subCategoryFocused = false;
   bool _CategoryFocused = false;
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void _logout(BuildContext context) async {
-    await _auth.signOut();
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove('uid');
-    prefs.remove('email');
-    prefs.remove('subCategory');
-    prefs.remove('isLoggedIn');
-
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const LoginPage()));
-  }
-
   List<DropDownValueModel> categoryList = [
-  const DropDownValueModel(name: 'ACADEMICS', value: 'ACADEMICS'),
-  const DropDownValueModel(name: 'ADMISSION', value: 'ADMISSION'),
-  const DropDownValueModel(name: 'COE', value: 'COE'),
-  const DropDownValueModel(name: 'COURSE REGISTRATION', value: 'COURSE REGISTRATION'),
-  const DropDownValueModel(name: 'CTS', value: 'CTS'),
-  const DropDownValueModel(name: 'HOSTEL', value: 'HOSTEL'),
-  const DropDownValueModel(name: 'HOSTEL REGISTRATION', value: 'HOSTEL REGISTRATION'),
-  const DropDownValueModel(name: 'HR', value: 'HR'),
-  const DropDownValueModel(name: 'MELITE', value: 'MELITE'),
-  const DropDownValueModel(name: 'OTHERS', value: 'OTHERS'),
-  const DropDownValueModel(name: 'P2P', value: 'P2P'),
-  const DropDownValueModel(name: 'PAT PLACEMENT', value: 'PAT PLACEMENT'),
-  const DropDownValueModel(name: 'STUDENT WELFARE', value: 'STUDENT WELFARE'),
-  const DropDownValueModel(name: 'VIT EVENTS', value: 'VIT EVENTS'),
-  const DropDownValueModel(name: 'VIT WEBSITE', value: 'VIT WEBSITE'),
-  const DropDownValueModel(name: 'VITIAN APP', value: 'VITIAN APP'),
-  const DropDownValueModel(name: 'VTOP', value: 'VTOP'),
-];
-
+    const DropDownValueModel(name: 'ACADEMICS', value: 'ACADEMICS'),
+    const DropDownValueModel(name: 'ADMISSION', value: 'ADMISSION'),
+    const DropDownValueModel(name: 'COE', value: 'COE'),
+    const DropDownValueModel(
+        name: 'COURSE REGISTRATION', value: 'COURSE REGISTRATION'),
+    const DropDownValueModel(name: 'CTS', value: 'CTS'),
+    const DropDownValueModel(name: 'HOSTEL', value: 'HOSTEL'),
+    const DropDownValueModel(
+        name: 'HOSTEL REGISTRATION', value: 'HOSTEL REGISTRATION'),
+    const DropDownValueModel(name: 'HR', value: 'HR'),
+    const DropDownValueModel(name: 'MELITE', value: 'MELITE'),
+    const DropDownValueModel(name: 'OTHERS', value: 'OTHERS'),
+    const DropDownValueModel(name: 'P2P', value: 'P2P'),
+    const DropDownValueModel(name: 'PAT PLACEMENT', value: 'PAT PLACEMENT'),
+    const DropDownValueModel(name: 'STUDENT WELFARE', value: 'STUDENT WELFARE'),
+    const DropDownValueModel(name: 'VIT EVENTS', value: 'VIT EVENTS'),
+    const DropDownValueModel(name: 'VIT WEBSITE', value: 'VIT WEBSITE'),
+    const DropDownValueModel(name: 'VITIAN APP', value: 'VITIAN APP'),
+    const DropDownValueModel(name: 'VTOP', value: 'VTOP'),
+  ];
 
   void _submitComplaint() async {
     if (_formKey.currentState!.validate()) {
       final User? user = FirebaseAuth.instance.currentUser;
 
       if (user != null) {
-        final String courtId = user.uid;
-        final String courtEmail = user.email!;
-
+        final String userId = user.uid;
         final String Category = _CategoryController.text.trim();
         final String deScription = _deScriptionController.text.trim();
-        final String subCategory = _subCategoryController.text.trim();
 
         try {
-          final QuerySnapshot existingComplaints = await FirebaseFirestore
-              .instance
-              .collection('complaints')
-              .where('courtId', isEqualTo: courtId)
-              .where('deScription', isEqualTo: deScription)
-              .get();
-
-          if (existingComplaints.docs.isNotEmpty) {
-            for (QueryDocumentSnapshot doc in existingComplaints.docs) {
-              await FirebaseFirestore.instance
-                  .collection('complaints')
-                  .doc(doc.id)
-                  .delete();
-            }
-          }
-
-          final QuerySnapshot lawyerQuery = await FirebaseFirestore.instance
-              .collection('users')
-              .where('email', isEqualTo: Category)
-              .get();
-
-          if (lawyerQuery.docs.isNotEmpty) {
-            final String lawyerName = lawyerQuery.docs[0]['name'];
-
-            final DocumentSnapshot userData = await FirebaseFirestore.instance
-                .collection('users')
-                .doc(courtId)
-                .get();
-
-            final String judgeName = userData['name'];
-
-            await FirebaseFirestore.instance.collection('complaints').add({
-              'judgeName': judgeName,
-              'courtId': courtId,
-              'courtEmail': courtEmail,
-              'subCategory': subCategory,
-              'deScription': deScription,
-              'Category': Category,
-            });
-
-            _deScriptionController.clear();
-            _subCategoryController.clear();
-            _CategoryController.clear();
-          }
+          CollectionReference complaints =
+              FirebaseFirestore.instance.collection('complaints');
+          Map<String, dynamic> data = {
+            "userId": userId,
+            "Category": Category,
+            "deScription": deScription,
+          };
+          await complaints.add(data);
+          _deScriptionController.clear();
+          _CategoryController.clear();
         } catch (error) {
           print('Error: $error');
         }
@@ -206,69 +148,29 @@ class _ComplaintsFormState extends State<ComplaintsForm> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          TextFormField(
-            controller: _CategoryController,
-            decoration: InputDecoration(
-              labelText: 'Category',
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: _CategoryFocused ? 3 : 1,
-                  color: Colors.lightBlueAccent,
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              filled: true,
-              fillColor: Colors.grey[900],
-              icon: const Icon(
-                Icons.email,
-                color: Colors.lightBlueAccent,
+          DropDownTextField(
+            textFieldDecoration: const InputDecoration(
+              icon: Icon(
+                Icons.category,
+                color: Colors.blue,
               ),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the category';
-              }
-              return null;
-            },
-            onTap: () {
-              setState(() {
-                _CategoryFocused = true;
-                _subCategoryFocused = false;
-                _deScriptionFocused = false;
-              });
-            },
-          ),
-          const SizedBox(height: 10),
-          TextFormField(
-            controller: _subCategoryController,
-            decoration: InputDecoration(
-              labelText: 'Sub-Category',
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: _subCategoryFocused ? 3 : 1,
-                  color: Colors.pink,
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              filled: true,
-              fillColor: Colors.grey[900],
-              icon: const Icon(
-                Icons.numbers,
-                color: Colors.pink,
-              ),
+            controller: _cnt, // Use the SingleValueDropDownController
+            clearOption: true,
+            searchDecoration: const InputDecoration(
+              hintText: 'Select a category',
             ),
             validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the subCategory';
+              if (value == null) {
+                return 'Please select a category';
+              } else {
+                return null;
               }
-              return null;
             },
-            onTap: () {
-              setState(() {
-                _deScriptionFocused = false;
-                _subCategoryFocused = true;
-                _CategoryFocused = false;
-              });
+            dropDownItemCount: categoryList.length,
+            dropDownList: categoryList,
+            onChanged: (val) {
+              // Handle the selected category here if needed
             },
           ),
           const SizedBox(height: 10),
@@ -299,7 +201,6 @@ class _ComplaintsFormState extends State<ComplaintsForm> {
             onTap: () {
               setState(() {
                 _deScriptionFocused = true;
-                _subCategoryFocused = false;
                 _CategoryFocused = false;
               });
             },
