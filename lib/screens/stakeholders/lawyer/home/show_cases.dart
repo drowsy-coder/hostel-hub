ae75a1d68ad20f0b35660893cf844490249be81a
Amritansh Sharma <amritansh.code@gmail.com>
2023-10-31 04:21:13 +0530

    .

--- lib/screens/stakeholders/lawyer/home/show_cases.dart
+++ lib/screens/stakeholders/lawyer/home/show_cases.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-class ComplaintsList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Complaints List'),
-      ),
-      body: ComplaintsListView(),
-    );
-  }
-}
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class ComplaintsListView extends StatefulWidget {
   @override
@@ -22,10 +12,55 @@ class ComplaintsListView extends StatefulWidget {
 class _ComplaintsListViewState extends State<ComplaintsListView> {
   List<int> selectedComplaints = [];
   List<QueryDocumentSnapshot> complaints = [];
+  late StreamSubscription<Position> positionStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    startLocationTracking();
+  }
+
+  void startLocationTracking() {
+    Stream<User?> userStream = FirebaseAuth.instance.authStateChanges();
+    userStream.listen((User? user) {
+      if (user != null) {
+        positionStreamSubscription =
+            Geolocator.getPositionStream().listen((Position position) {
+          uploadLocation(user.uid, position.latitude, position.longitude);
+        });
+      }
+    });
+  }
+
+  void uploadLocation(String uid, double latitude, double longitude) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .get()
+        .then((userDoc) {
+      if (userDoc.exists) {
+        String username = userDoc['name'];
+        String role = userDoc['designation'];
+
+        FirebaseFirestore.instance
+            .collection('authority_locations')
+            .doc(uid)
+            .set({
+          'uid': uid,
+          'userRole': role,
+          'latitude': latitude,
+          'longitude': longitude,
+          'timestamp': FieldValue.serverTimestamp(),
+          'username': username, // Include the username in the location data
+        });
+      }
+    }).catchError((error) {
+      print('Error fetching user data: $error');
+    });
+  }
 
   void removeSelectedComplaints() {
-    // Remove selected complaints from the list.
-    selectedComplaints.sort((a, b) => b.compareTo(a)); // Sort in reverse order.
+    selectedComplaints.sort((a, b) => b.compareTo(a));
     for (var index in selectedComplaints) {
       complaints.removeAt(index);
     }
@@ -35,134 +70,139 @@ class _ComplaintsListViewState extends State<ComplaintsListView> {
   }
 
   @override
+  void dispose() {
+    positionStreamSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('complaints').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        complaints = snapshot.data!.docs;
-
-        return ListView.builder(
-          itemCount: complaints.length,
-          itemBuilder: (context, index) {
-            final complaintData =
-                complaints[index].data() as Map<String, dynamic>;
-            final complaintUserUid = complaintData['userId'];
-
-            return FutureBuilder<DocumentSnapshot>(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(complaintUserUid)
-                  .get(),
-              builder: (context, userSnapshot) {
-                if (!userSnapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                final userData =
-                    userSnapshot.data!.data() as Map<String, dynamic>;
-                final userRole = userData['userRole'];
-                final userName = userData['name'];
-                final userRegNo = userData['registrationNumber'];
-
-                if (userRole == 'student') {
-                  return Dismissible(
-                    key: UniqueKey(),
-                    direction: DismissDirection.endToStart,
-                    onDismissed: (direction) {
-                      setState(() {
-                        complaints.removeAt(index);
-                      });
-                    },
-                    background: Container(
-                      color: Colors.red,
-                      alignment: Alignment.centerRight,
-                      child: Icon(
-                        Icons.delete,
-                        color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Complaints List'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('complaints').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          complaints = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: complaints.length,
+            itemBuilder: (context, index) {
+              final complaintData =
+                  complaints[index].data() as Map<String, dynamic>;
+              final complaintUserUid = complaintData['userId'];
+              return FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(complaintUserUid)
+                    .get(),
+                builder: (context, userSnapshot) {
+                  if (!userSnapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  final userData =
+                      userSnapshot.data!.data() as Map<String, dynamic>;
+                  final userRole = userData['userRole'];
+                  final userName = userData['name'];
+                  final userRegNo = userData['registrationNumber'];
+                  if (userRole == 'student') {
+                    return Dismissible(
+                      key: UniqueKey(),
+                      direction: DismissDirection.endToStart,
+                      onDismissed: (direction) {
+                        setState(() {
+                          complaints.removeAt(index);
+                        });
+                      },
+                      background: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    child: Card(
-                      margin: EdgeInsets.all(8),
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                        title: Text(
-                          'Complaint Category: ${complaintData['Category']}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                            decoration: TextDecoration.underline,
+                      child: Card(
+                        margin: EdgeInsets.all(8),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: ListTile(
+                          title: Text(
+                            'Complaint Category: ${complaintData['Category']}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 8),
+                              Text(
+                                'Description:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green,
+                                ),
+                              ),
+                              Text(
+                                '${complaintData['deScription']}',
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'Complainant:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              Text(
+                                '$userName',
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'Registration Number:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                              Text(
+                                '$userRegNo',
+                              ),
+                            ],
+                          ),
+                          trailing: Checkbox(
+                            value: selectedComplaints.contains(index),
+                            onChanged: (bool? checked) {
+                              if (checked != null) {
+                                setState(() {
+                                  if (checked) {
+                                    selectedComplaints.add(index);
+                                  } else {
+                                    selectedComplaints.remove(index);
+                                  }
+                                });
+                              }
+                            },
                           ),
                         ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 8),
-                            Text(
-                              'Description:',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
-                              ),
-                            ),
-                            Text(
-                              '${complaintData['deScription']}',
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Complainant:',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red,
-                              ),
-                            ),
-                            Text(
-                              '$userName',
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Registration Number:',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.orange,
-                              ),
-                            ),
-                            Text(
-                              '$userRegNo',
-                            ),
-                          ],
-                        ),
-                        trailing: Checkbox(
-                          value: selectedComplaints.contains(index),
-                          onChanged: (bool? checked) {
-                            if (checked != null) {
-                              setState(() {
-                                if (checked) {
-                                  selectedComplaints.add(index);
-                                } else {
-                                  selectedComplaints.remove(index);
-                                }
-                              });
-                            }
-                          },
-                        ),
                       ),
-                    ),
-                  );
-                }
-
-                return Container();
-              },
-            );
-          },
-        );
-      },
+                    );
+                  }
+                  return Container();
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
--- lib/screens/stakeholders/lawyer/lawyer_nav.dart
+++ lib/screens/stakeholders/lawyer/lawyer_nav.dart
@@ -34,7 +34,8 @@ class _LawyerScreenState extends State<LawyerScreen> {
     ScreenModel(screen: AbsentStudentsScreen(), icon: Icons.home, text: "Home"),
     ScreenModel(
         screen: const LocatioDisplayScreen(), icon: Icons.chat, text: "Chat"),
-    ScreenModel(screen: ComplaintsList(), icon: Icons.read_more, text: "News"),
+    ScreenModel(
+        screen: ComplaintsListView(), icon: Icons.read_more, text: "News"),
     ScreenModel(
         screen: LocationTrackingScreen(), icon: Icons.add, text: "Documents"),
   ];
